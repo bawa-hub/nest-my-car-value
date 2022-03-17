169aae68990c247e10ec289edcd9ee3f9e0d7f90
bawa-hub <itsworkingmind@gmail.com>
2022-03-17 09:40:38 +0530

    interceptor and serializer added

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: ClassConstructor) {}
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToClass(this.dto, data, { excludeExtraneousValues: true });
+      }),
+    );
+  }
+}
+
+export function Serialize(dto: any) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
